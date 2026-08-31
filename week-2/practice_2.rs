1f6ad80d453547a7dffae63c6bc70edcd9a0cf13
kuripau <ugochkwuyerem.odera-onyebuagu@pau.edu.ng>
2026-08-31 17:18:27 +0100

    (h/w no 1)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,7 @@
+fn main(){
+	println!(); // prints just a
+	println!("hello "); //prints
+	println!("format {} arguemnets", "some" ); //prints format some arguments
+	println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo" );
+	
+}
